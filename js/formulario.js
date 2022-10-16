efc7b8f6978d5909d0a12c1b146ba2e332b6c5fd
Di3goVM <diego.vdm94@gmail.com>
2022-10-15 23:04:05 -0300

    nuevo diseño

---
+++ js/formulario.js
@@ -0,0 +1,14 @@
+//FORMULARIO HOME-CREW
+
+let inputs = document.getElementsByClassName('formulario__input');
+
+for (let i = 0; i < inputs.length; i++) {
+	inputs[i].addEventListener('keyup', function() {
+		if (this.value.length>=1) {
+			this.nextElementSibling.classList.add('fijar');
+		}
+		else {
+			this.nextElementSibling.classList.remove('fijar');
+		}
+	});
+};
